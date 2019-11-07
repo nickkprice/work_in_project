c7c8840280fd338a7502e6ad4b7f973e50e87ade
EvanKuehr <evan.kuehr@colorado.edu>
2019-11-06 23:27:24 -0700

    Implemented a rudimentary login system (server side)

--- cuswapserver.js
+++ cuswapserver.js
@@ -45,11 +45,65 @@ let db = pgp(dbConfig);
 app.use(express.static(__dirname + '/')); // This line is necessary for us to use relative paths and access our resources directory
 app.set('view engine', 'ejs'); //using this view engine to res.sendFile html pages
 app.engine('html', require('ejs').renderFile);
+var ejs = require('ejs');
+var fs = require('fs');
 
 // home page
-app.get('/homepage.html', function(req, res) {
+app.get('/homepage', function(req, res) {
+  // var loggedIn = req.session.loggedIn;
 	res.sendFile(__dirname+'/views/homepage.html',{ //sends the client the homepage file from the views folder
 	});
+});
+
+//login page
+app.get('/login', function(req, res) {
+	res.sendFile(__dirname+'/views/login.html',{
+	});
+});
+
+app.post('/login/submitLogin', function(req, res) {
+  var inputUsername = req.body.uName;
+  var inputPassword = req.body.uPassword;
+	//This query will return the user id of the user with username/pass combo that was entered on login page
+  var loginquery1 = 'SELECT user_id FROM "user" WHERE username=\'' + inputUsername + '\' AND password=\'' + inputPassword + '\';';
+
+  db.task('get-everything', task => {
+      return task.batch([
+          task.any(loginquery1),
+      ]);
+  })
+  .then(data => {
+		//Result of query:
+		// console.log(data[0][0]);
+
+    //need to generate a cookie and send to user
+
+    // fs.readFile(__dirname + '/views/homepage.html', 'utf-8', (err, html) => {
+    //   res.send(ejs.render(html, JSON.stringify(loggedIn)))
+    //The above code works but will not redirect to homepage url, it merely stays on login/submitLogin and renders homepage
+
+		if(data[0][0]) //if user with that user/pass combo was found by sql query
+		{
+			console.log("Authenticated");
+			var loggedIn = true;
+			// req.session.loggedIn = loggedIn;
+			res.sendFile(__dirname+'/views/homepage.html',{ //stays on login/submitLogin url, need to redirect instead?
+				//also need to be able to send the loggedIn variable to homepage which will be used by clientside JS
+			});
+		}
+		else //failed login
+		{
+			res.sendFile(__dirname+'/views/login.html',{
+			});
+		}
+
+  })
+  .catch(error => {
+      // display error message in case an error
+          console.log(error);
+          res.sendFile(__dirname+'/views/login.html',{
+  		});
+  });
 });
 
 app.listen(8080);
