d2a56e6b86891d1ebe96d93b7acc5f11f6af9648
SeerOfDreams <kaylee.e.engelhardt@gmail.com>
2019-11-12 16:41:41 -0700

    fixed the nodejs syntax

--- database/node.js
+++ database/node.js
@@ -1,14 +1,14 @@
 /*check if user exists nodejs sql query*/
-var findUser = "SELECT * FROM 'user' WHERE username = " + username_variable + ";"
+var findUser = "SELECT * FROM \"user\" WHERE username = '" + username_variable + "';"
 /*find users posts nodejs sql query*/
-var findUserPosts = "SELECT * FROM 'post' WHERE username = " + username_variable + ";"
+var findUserPosts = "SELECT * FROM \"post\" WHERE username = '" + username_variable + "';"
 /*sign in nodejs sql query*/
-var signin = "SELECT * FROM 'user' WHERE username = " + username_variable + " AND password = " + password_variable + ";"
+var signin = "SELECT * FROM \"user\" WHERE username = '" + username_variable + "' AND password = '" + password_variable + "';"
 /*new filter query nodejs sql*/
-var newFilter = "SELECT * FROM 'post' WHERE complete = FALSE ORDER BY date_created DESC;"
+var newFilter = "SELECT * FROM \"post\" WHERE complete = FALSE ORDER BY date_created DESC;"
 /*any filter other than just new nodejs sql*/
 var inputArray = [0,0,0,0,0,0,0];
-var query = "SELECT * FROM 'post' WHERE complete = FALSE ";
+var query = "SELECT * FROM \"post\" WHERE complete = FALSE ";
 if(inputArray[0] == 1){
   query += "AND tag_array[1] = TRUE "
 }
